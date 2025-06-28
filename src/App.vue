2ac6ef63b8441a453bfbbcf212ea4ea4f2f5e630
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-28 06:27:23 +0100

    ENV Added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
 	<div>
+    <h3>{{ appName }}</h3>
 		<form>
 			<div>
-				<input type="text" placeholder="GitHub Username" />
+				<input
+					type="text"
+					placeholder="GitHub Username"
+					@blur="isGitHubUsernameAvailable"
+					v-model="username"
+				/>
+				{{ available }}
 			</div>
 			<div>
-				<input type="text" placeholder="Work Space Folder" />
+				<input type="text" placeholder="Work Space Folder" v-model="workspace" />
 			</div>
 
 			<button>Save</button>
@@ -16,6 +23,21 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const workspace = ref("");
+const username = ref("");
+let available = ref("");
+
+async function isGitHubUsernameAvailable() {
+	const { data } = await axios.get(
+		`http://localhost:2001/check-username?username=${username.value}`
+	);
+
+	data ? (available = "✅ Available") : (available = "❌ Taken");
+}
+
 const socket = new WebSocket(`ws://localhost:2001/`);
 
 socket.addEventListener("open", () => {
@@ -25,6 +47,8 @@ socket.addEventListener("open", () => {
 
 socket.addEventListener("message", (event) => {
 	const data = JSON.parse(event.data);
+
+	workspace.value = data.dir;
 	if (data.type === "server-dir") {
 		console.log(`Server __dirname:\n${data.dir}\n`);
 	}
@@ -33,4 +57,6 @@ socket.addEventListener("message", (event) => {
 socket.addEventListener("close", () => {
 	console.log("WebSocket connection closed.");
 });
+
+const appName = import.meta.env.VITE_APP_NAME
 </script>
